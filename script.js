c07d92899a60c4a897c5be98594fd54f6e60492b
Adharv Arun <adharvarun.10@gmail.com>
2025-03-27 14:54:51 +0400

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    AOS.init({
-        duration: 1000,
-        once: true,
-        offset: 100,
-        easing: 'ease-out-cubic'
-    });
-});
