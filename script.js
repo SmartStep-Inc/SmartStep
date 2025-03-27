77734446299442f22d9f3ca4a995b671b86ec866
Adharv Arun <adharvarun.10@gmail.com>
2025-03-27 15:01:41 +0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+document.addEventListener('DOMContentLoaded', function() {
+    AOS.init({
+        duration: 1000,
+        once: true,
+        offset: 100,
+        easing: 'ease-out-cubic'
+    });
+});
